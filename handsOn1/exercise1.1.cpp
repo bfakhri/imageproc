c68ffdbb4c9338eecd4cbb09c34e44c50fb5f8c3
Bijan Fakhri <bfakhri@asu.edu>
2017-02-01 11:15:32 -0700

    Cleaned up 1.1

--- handsOn1/exercise1.1.cpp
+++ handsOn1/exercise1.1.cpp
@@ -1,4 +1,6 @@
-//
+// Bijan Fakhri
+// Exercise 1.1
+// 2-2-17
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
@@ -8,31 +10,31 @@ using namespace cv;
 
 int main()
 {
-		VideoCapture cap(0); // open the default camera
-		if (!cap.isOpened())  // check if we succeeded
+		VideoCapture cap(0);	// open the default camera
+		if (!cap.isOpened())	// check if we succeeded
 			return -1;
 
-		Size S = Size((int)cap.get(CV_CAP_PROP_FRAME_WIDTH)/2,    // Acquire input size
-			(int)cap.get(CV_CAP_PROP_FRAME_HEIGHT)/2);
+		// Acquire Input Size
+		Size S = Size((int)cap.get(CV_CAP_PROP_FRAME_WIDTH)/2, (int)cap.get(CV_CAP_PROP_FRAME_HEIGHT)/2);
 
 		VideoWriter outputVideo;    // open the output
-		//outputVideo.open("Record.avi", -1, cap.get(CV_CAP_PROP_FPS), S, true);
                
-// For Windows, if the above does not work, use the following command instead:
-		//outputVideo.open("Record.avi", CV_FOURCC('D', 'I', 'V', '3'), cap.get(CV_CAP_PROP_FPS), S, true);
+		// For Windows, if the above does not work, use the following command instead:
 		outputVideo.open("Record.avi", CV_FOURCC('D', 'I', 'V', '3'), cap.get(CV_CAP_PROP_FPS), S, true);
-// For MAC OS, if the above does not work, use the following instruction instead:
+		// For MAC OS, if the above does not work, use the following instruction instead:
                 // outputVideo.open("Record.avi", CV_FOURCC('S', 'V', 'Q', '3'), 30.0, S, true);
 
-		if (!outputVideo.isOpened())
-		{
+		if (!outputVideo.isOpened()){
 			cout << "Could not open the output video for write: " << endl;
 			return -1;
 		}
 
-		namedWindow("Video", 1);
+		// Create two windows
+		namedWindow("Original Video", 1);
+		namedWindow("Downsampled Video", 1);
 		cout << "Start Reading and Writing" << endl;
 
+		// Capture 200 Frames
 		int cnt = 0;
 		while (cnt<200)
 		{
@@ -45,10 +47,18 @@ int main()
 			Mat frameDown = cvarrToMat(downSmp);
 			outputVideo << frameDown;  // store the frame to the output video file
 
-			imshow("Video", frameDown);
+			// Display the image
+			imshow("Original Video", frame);
+			imshow("Downsampled Video", frameDown);
+			// End the streams if key is pressed
 			if (waitKey(30) >= 0) 
 				break;
+
+			// Release the memory alloted to the new image
+			cvReleaseImage(&downSmp);
+
 			cnt++;
+
 		}
 		// the camera will be deinitialized automatically in VideoCapture destructor
 		cout << "Finished Writing" << endl;
